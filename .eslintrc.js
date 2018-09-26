79d4a03a8bde8041928618799ec88344816838dc
rot1024 <aayhrot@gmail.com>
2018-09-26 19:03:02 +0900

    use jest instead of karma

--- .eslintrc.js
+++ .eslintrc.js
@@ -86,7 +86,15 @@ module.exports = {
     {
       files: ["src/**/*.test.js"],
       env: {
-        jasmine: true,
+        "jest/globals": true,
+      },
+      plugins: ["jest"],
+      rules: {
+        "jest/no-disabled-tests": 1,
+        "jest/no-focused-tests": 2,
+        "jest/no-identical-title": 2,
+        "jest/prefer-to-have-length": 1,
+        "jest/valid-expect": 2,
       },
     },
   ],
